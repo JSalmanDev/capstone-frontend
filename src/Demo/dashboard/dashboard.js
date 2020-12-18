cd3447a079b143797d9f2239177099ea9c345f65
JSalmanDev <jamil@isalman.net>
2020-12-18 05:36:56 +0500

    categories list display

--- src/Demo/dashboard/dashboard.js
+++ src/Demo/dashboard/dashboard.js
@@ -3,7 +3,11 @@ import { Row, Col, Card } from 'react-bootstrap';
 import Aux from "../../hoc/_Aux";
 import 'react-table/react-table.css';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
-import Loader from '../../App/layout/Loader'
+import Loader from '../../App/layout/Loader';
+import axios from 'axios';
+import config from '../../config';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/userActions';
 
 class Dashboard extends React.Component {
 
@@ -13,13 +17,55 @@ class Dashboard extends React.Component {
             isLoading: false,
 			isValid: {
                 value: false,
-                text: ''   
+                text: '',
+                name: ''   
             },
-            
         }
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        axios.get(`${config.prod}/api/category/list`)
+            .then(res => {
+                let sideNav = [
+                    {
+                        id: 'navigation',
+                        title: 'Categories',
+                        type: 'group',
+                        icon: 'icon-navigation',
+                        children: [ ]
+                    }
+                ];
+                let children = [{
+                    id: 'dashboard',
+                    title: 'Dashboard',
+                    type: 'item',
+                    url: '/dashboard',
+                    icon: 'feather icon-home',
+                }];
+
+                res.data.data.forEach(elem => {
+                    children.push(
+                        {
+                            id: `${elem.id}`,
+                            title: `${elem.title}`,
+                            type: 'item',
+                            url: `/project?project=${elem.id}`,
+                            icon: 'feather icon-box',
+                        }
+                    );
+                });
+
+                sideNav[0].children = children;
+                this.props.sideNavigation(sideNav);
+                this.setState({ isLoading: false, isValid: { value: false, text: '', name: '' }});
+            })
+            .catch(err => {
+                console.log('Error: ', err);
+                this.setState({ isLoading: false, isValid: { value: true, text: 'Internal Server Error. Try again by Reload.', name: 'server_error' } });
+                this.createNotification('error', 'Internal Server Error');
+            });
+    }
     
     createNotification = (type, value) => {
         switch (type) {
@@ -47,10 +93,16 @@ class Dashboard extends React.Component {
                     <NotificationContainer/>
                     <Col>
                         <Card>
-                            <Card.Header>
-                                <Card.Title as="h5">Welcome User</Card.Title>
-                            </Card.Header>
                             <Card.Body>
+                                <Row>
+                                    <Col>
+                                        { !this.state.isLoading && this.state.isValid.value ? 
+                                            <h5 className="text-center" style={{ color: 'red' }}>{this.state.isValid.text}</h5>
+                                            :
+                                            <h5 className="text-center">Select a Category to view it's questions.</h5>
+                                        }
+                                    </Col>
+                                </Row>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -60,4 +112,5 @@ class Dashboard extends React.Component {
     }
 }
 
-export default Dashboard;
+
+export default connect(null, actions)(Dashboard);
