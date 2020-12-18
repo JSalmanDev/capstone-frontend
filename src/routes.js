274ff98ed02b3ee15c3375fff0a5c4412d2ae8cb
JSalmanDev <jamil@isalman.net>
2020-12-18 12:19:57 +0500

    connect answers page to app

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ global.jQuery = $;
 //Users
 const Dashboard = React.lazy(() => import("./Demo/dashboard/dashboard"));
 const Project = React.lazy(() => import("./Demo/Questions/Questions"));
+const Answer = React.lazy(() => import("./Demo/Answers/Answers"));
 
 //Not Found
 const NotFound = React.lazy(() => import("./Demo/NotFound/NotFound"));
@@ -17,6 +18,7 @@ const routes = [
     //users routes
     { path: '/dashboard', exact: true, role:'user', user: true, component: RequireAuth(Dashboard) },
     { path: '/project', exact: true, role:'user', user: true, component: RequireAuth(Project) },
+    { path: '/project/question', exact: true, role:'user', user: true, component: RequireAuth(Answer) },
     { path: '/not-found', exact: true, role:'public', component: RequireAuth(NotFound) }
 ];
 
